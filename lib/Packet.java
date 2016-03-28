485a7b40f9144db52b603a69b6d974f775b99e9c
vickykatara <vicky.p.katara@gmail.com>
2016-03-28 14:34:11 -0400

    Files Added

--- lib/Packet.java
+++ lib/Packet.java
@@ -7,16 +7,16 @@ package lib;
  * */
 
 public class Packet {
-	private int option;
+	private int type;
 	private String data;
 	
-	public Packet(int option, String data){
-		this.option = option;
+	public Packet(int type, String data){
+		this.type = type;
 		this.data = data;
 	}
 	
 	public int getOption(){
-		return this.option;
+		return this.type;
 	}
 	
 	public String getData(){
@@ -26,21 +26,21 @@ public class Packet {
 	public Packet(String payload){
 		String[] arr = payload.split("\\|");
 		//System.out.println(Arrays.toString(arr));
-		this.option = Integer.parseInt(arr[2]);
+		this.type = Integer.parseInt(arr[2]);
 		this.data = arr[6];
 	}
 	
 	public String toString(){
-		return option+":"+data;
+		return type+":"+data;
 	}
 	
 	public String getPayload(){
-		return "|option|"+option+"|/option||data|"+data+"|/data|";
+		return "|type|"+type+"|/type||data|"+data+"|/data|";
 	}
 	
 	public static void main(String[] args) {
-		Packet p = new Packet("|option|2|/option||data|1,Intro to Algos,8,mp3;2,Intro to networks,5,mp4;3,Intro to AI,6,mp4|data|");
-		System.out.println(p);
+		//Packet p = new Packet("|option|2|/option||data|1,Intro to Algos,8,mp3;2,Intro to networks,5,mp4;3,Intro to AI,6,mp4|data|");
+		//System.out.println(p);
 		//String s = ":abc:xyz";
 		//System.out.println(Arrays.toString(s.split(":")));
 	}
