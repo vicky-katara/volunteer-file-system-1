3b3df1e9e74d6f7bd58f528afc8c4d3237aa39cc
vickykatara <vicky.p.katara@gmail.com>
2016-04-02 21:18:43 -0400

    RequestReceiver complete but not tested

---
+++ lib/Chunk.java
@@ -0,0 +1,51 @@
+package lib;
+
+import java.util.Arrays;
+
+public class Chunk implements Comparable<Chunk>{
+	String chunkName;
+	String byteString;
+	
+	public Chunk(byte[] b){
+		this.byteString = Arrays.toString(b);
+	}
+	Chunk(String byteString){
+		this.byteString = byteString;
+	}
+	public Chunk(String chunkName, String byteString){
+		this.chunkName = chunkName;
+		this.byteString = byteString;
+	}
+	
+	public String getChunkName(){
+		return this.chunkName;
+	}
+	
+	public String toString(){
+		return (byteString);//.replace(oldChar, newChar);
+	}
+	
+	public byte[] returnBytes(){
+		try{
+			String[] stringByte = byteString.replace("[", "").replace("]", "").split(", ");
+			byte[] byteArr= new byte[stringByte.length];
+			for(int index=0;index < byteArr.length; index++)
+				byteArr[index] = Byte.parseByte(stringByte[index]);
+			return byteArr;
+		}
+		catch(NumberFormatException nfe){
+			System.err.print(Thread.currentThread().getName()+" has encountered a NumberFormatException in Chunk.returnBytes:");
+			System.err.println("It tried to parse "+this.byteString);
+		}
+		return null;
+	}
+	@Override
+	public int compareTo(Chunk o) {
+		return this.chunkName.compareTo(o.chunkName);
+	}
+	public static void main(String[] args) {
+		Chunk c = new Chunk(new byte[]{75, 78, -111, -59, -19, 33, 68, -11, 81, 48, -93, -48, -14, -74, 76, -114, -123, 29, -34, 61, 99, 70, 105, -50, 49, 8, 0, 3, 81, -104, 0, 15, 47, 8, 48, -16, 120, -59, -30, 67, 9, -122, -52, 124, 20, 11, 6, -126, -32, -26, 70, -70, 20, -101, 48, 71, 40, 113, 127, 33, -78, -108, -92, 43, -68, 87, 15, 1, -71, -32, -49, 72, 43, 41, 71, -97, 51, 116, -3, 106, -27, -84, -4, -54, 75, 93, -87, 42, -122, 38, 83, -83, -49, -61, -84, 0, 56, -48, -28, 10, 95, -4, -87, 117, 69, -55, -55, 102, -1, -81, 19, 112, 110, 126, -123, -37, 55, -48, -125, -69, 5, -96, 108, -46, 119, 72, 55, 45, 83, 71, 31, 29, 119, 126, -19, 101, -9, -40, -51, -104, 67, -75, 41, -87, -79, 19, 15, -113, -101, -65, 59, -61, 98, 38, 121, -73, -50, -6, -3, -97, 39, 109, -79, -97, -73, 127, -116, -3, 2, -95, -105, 12, -118, 41, -90, 24, -119, -30, 12, -103, 34, -29, 60, -101, 65, 3, 0, 116, -24, -63, -58, 80, 71, -98, 1, -101, -84, 126, 7, 24, 65, 2, -54, -64, 68, 69, -124, -75, 81, -32, 42, 50, -50, 92, 117, -3, 24, 105, -103, 77, 85, -128, 31, -64, 38, 88, 32, 72, 42, 17, -106, -62, -64, -76, -128, 106, -44, 72, -29, 40, 64, -120, 67, 49, -61, 44, -112, -26, -53, 86, -118, -33, 18, 69, -36, 98});
+		System.out.println(c.byteString.getClass());
+		System.out.println(Arrays.toString(c.returnBytes()));
+	}
+}
--- requestReceiver/Servant.java
+++ requestReceiver/Servant.java
@@ -2,14 +2,14 @@ package requestReceiver;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.Socket;
 import java.net.UnknownHostException;
 
+import lib.Chunk;
 import lib.Peer;
 import lib.Packet;
 import lib.SenderReceiver;
@@ -18,41 +18,74 @@ public class Servant extends Thread{
 
 	DatagramPacket udpDatagram;
 	DatagramSocket socketOnWhichToSend;
-	String pathToVideosDir;
+	String pathToDir;
 	Servant(DatagramPacket fromRequestor, DatagramSocket onWhichToSend){
-//		this.udpDatagram = fromRequestor;
-//		this.socketOnWhichToSend = onWhichToSend;
-//		pathToVideosDir = System.getProperty("user.home")+File.separatorChar+"Videos";
+		this.udpDatagram = fromRequestor;
+		this.socketOnWhichToSend = onWhichToSend;
+		pathToDir = System.getProperty("user.home")+File.separatorChar+"Chunks";
+		File pathDir = new File(pathToDir);
+		if (pathDir.isDirectory()==false)
+			pathDir.mkdir();
 	}
 	public void run() {
-		Packet option100Packet = null;
+		Packet receivedPacket = null;
 		//while(true){
 			try {
-//				option100Packet = new Packet(new String(udpDatagram.getData()));
-//				if(option100Packet.getOption()!=100)
-//					throw new Exception("option100Packet has option "+option100Packet.getOption()+" : Servant.run()");
-//				String[] splitted = option100Packet.getData().split(":");
-//				Chunk chunkToBeReturned = getRequestedChunk(splitted[0], Integer.parseInt(splitted[1]));
-//				//new SenderReceiver().sendMesssageOn(packetFromRequestor, new Packet(101, option100Packet.getData()+":"+chunkToBeReturned).getPayload());
-//				new SenderReceiver().sendUDPReply(socketOnWhichToSend, new Peer(udpDatagram.getAddress().getHostAddress(), udpDatagram.getPort()), new Packet(101, option100Packet.getData()+":"+chunkToBeReturned).getPayload());
+				receivedPacket = new Packet(new String(udpDatagram.getData()));
+				if(receivedPacket.getOption()==100) {
+					new SenderReceiver().sendUDPReply(socketOnWhichToSend, new Peer(udpDatagram.getAddress().getHostAddress(), udpDatagram.getPort()), new Packet(101, "").getPayload());
+				} else if(receivedPacket.getOption()==102) {
+					String chunkName = receivedPacket.getData();
+					Chunk chunkToBeReturned = getRequestedChunk(chunkName);
+					new SenderReceiver().sendUDPReply(socketOnWhichToSend, new Peer(udpDatagram.getAddress().getHostAddress(), udpDatagram.getPort()), new Packet(103, receivedPacket.getData()+":"+chunkToBeReturned).getPayload());
+				} else if(receivedPacket.getOption()==104) {
+					String[] splitted = receivedPacket.getData().split(":");
+					Chunk toBeStored = new Chunk(splitted[0], splitted[1]);
+					storeChunk(toBeStored);
+					new SenderReceiver().sendUDPReply(socketOnWhichToSend, new Peer(udpDatagram.getAddress().getHostAddress(), udpDatagram.getPort()), new Packet(105, splitted[0]).getPayload());
+				} else {
+					throw new Exception("receivedPacket has option "+receivedPacket.getOption()+" : Servant.run() --> "+receivedPacket);
+				}
 			}
 			catch(NullPointerException npe){
-				System.err.println("Problem with option100Packet: "+option100Packet);
 				npe.printStackTrace();
-				//currentThread().stop();
 			}
 			catch (Exception e) {e.printStackTrace();
 			}
-			
-		//}
 	}
 	
+	Chunk getRequestedChunk(String fileName){
+		try {
+			byte[] b = new byte[256];
+			RandomAccessFile raf = new RandomAccessFile(pathToDir+File.separatorChar+fileName, "r");// doesn't have to be done every time!
+			raf.read(b);
+			raf.close();
+			return new Chunk(b);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	void storeChunk(Chunk toBeStored){
+		FileOutputStream fos;
+		try {
+			fos = new FileOutputStream(pathToDir+File.separatorChar+toBeStored.getChunkName());
+			fos.write(toBeStored.returnBytes());
+			fos.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
 	public static void main(String[] args) throws UnknownHostException {
-		//System.out.println(System.getProperty("user.home")+File.separatorChar+"Videos");
-		//System.out.println(InetAddress.getByName("192.168.1.1").getHostAddress());
-		//byte[] size512 = "Vicky".getBytes();
-//		byte[] size512 = new byte[512];
-//		String s = new String(size512).trim();
-//		System.out.println(s.length());
+		
 	}
 }
