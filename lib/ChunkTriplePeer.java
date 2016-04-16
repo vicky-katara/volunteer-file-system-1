c562e202197cb6f1c4b70c97729ee49b215c0dd7
ssharm17 <ssharm17@ncsu.edu>
2016-04-16 14:45:07 -0400

    changes to requester

---
+++ lib/ChunkTriplePeer.java
@@ -0,0 +1,56 @@
+package lib;
+
+import java.io.Serializable;
+
+public class ChunkTriplePeer implements Serializable{
+	private static final long serialVersionUID = -2610681546566455003L;
+	String chunkName;
+	Peer one;
+	Peer two;
+	Peer three;
+
+	ChunkTriplePeer(String chunkName, Peer one, Peer two, Peer three){
+		this.chunkName = chunkName;
+		this.one = one;
+		this.two = two;
+		this.three = three;
+	}
+
+	public ChunkTriplePeer() {
+
+	}
+
+	public ChunkTriplePeer(String chunkName) {
+		this.chunkName = chunkName;
+	}
+
+	void setChunkName(String chunkName) {
+		this.chunkName = chunkName;
+	}
+
+	public void setPrimaryPeer(Peer x){
+		this.one = x;
+	}
+	public void setSecondaryPeer(Peer x){
+		this.two = x;
+	}
+	public void setTertiaryPeer(Peer x){
+		this.three = x;
+	}
+	
+	public void setPeerNumber(int number, Peer x){
+		if(number==1){
+			this.one = x;
+		} else if(number == 2){
+			this.two = x;
+		} else if(number ==3){
+			this.three = x;
+		} else {
+			System.err.println("Wrong number for peer index");
+		}
+	}
+
+	public String toString(){
+		return "["+chunkName+"-"+one+"-"+two+"-"+three+"]";
+	}
+}
